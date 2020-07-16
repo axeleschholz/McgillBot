2698887b56a91b5095d41168b84d477deaf57ba9
axeleschholz <55470307+axeleschholz@users.noreply.github.com>
2020-07-15 22:25:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -87,11 +87,9 @@ consents = ["you're all set!",
 @bot.event
 async def on_member_join(member):
     print(f"{member} has joined {member.guild.name}")
-    if member.guild.name == GUILD or member.guild.name == "McGill | Faculty of Education":
+    if member.guild.name == GUILD:
       place = get(member.guild.channels, name="consent")
-      if not place:
-        place = get(member.guild.channels, name="traffic")
-      joinmessage = f"Welcome {member.mention}! \n To get started, first read our rules in #information, and if you agree to abide by those rules, type **.iam Consenting** \n Then you'll want to verify your status as a McGill student in #verification. Feel free to peruse the rest of the announcements and information or message an Administrator/Moderator if you need any help!"
+      joinmessage = f"Welcome {member.mention}! \nTo get started, first read our rules in #information, and if you agree to abide by those rules, type **.I Consent** \nThen you'll want to verify your status as a McGill student in #verification. Feel free to peruse the rest of the announcements and information or message an Administrator/Moderator if you need any help!"
       await place.send(joinmessage, delete_after=60)
   
 @bot.command(name='kik', help='Kiks jim')
