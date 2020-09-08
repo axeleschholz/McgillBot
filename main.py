ec8a2b23fe6ff9b10fff251f171fc669644246e5
axeleschholz <55470307+axeleschholz@users.noreply.github.com>
2020-09-08 18:18:27 -0300

    email dependancies

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from discord.ext import commands
 from discord.utils import get
 import random
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 TOKEN = os.getenv('DISCORD_TOKEN')
