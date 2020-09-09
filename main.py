f3602dc5e532dc28e132ce662517064a04c7df44
axeleschholz <55470307+axeleschholz@users.noreply.github.com>
2020-09-08 22:20:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ async def on_member_join(member):
       info = get(guild.channels, name='information')
       message = "Welcome <@" + str(member.id) + ">!\nYou should give our rules a read at <#" + str(info.id) + ">.\nBy verifying yourself, you agree to our rules set out in <#" + str(info.id) + "> and failure to abide by the rules may result in a warning or ban.\nFeel free to peruse the rest of the announcements and information or message an Administrator/Moderator if you need any help!"
       
-     await member.send(message)
+      await member.send(message)
 
 
       
